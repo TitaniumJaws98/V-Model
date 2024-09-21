b177301163e663068b99cff0f6dd11952df2af4c
TitaniumJaws98 <braiyan.ramirez98@gmail.com>
2024-09-21 12:47:05 -0600

    Creacion de proyecto
    - Creación de proyecto Java para el punto de venta

---
+++ PuntoDeVentaBLRV/src/main/java/com/mycompany/puntodeventablrv/PuntoDeVentaBLRV.java
@@ -0,0 +1,15 @@
+package com.mycompany.puntodeventablrv;
+/**
+ *
+ * @autor Braiyan Leonardo Ramirez Vargas
+ * @archivo PuntoDeVenta.java
+ * @clase Verificación y validación de software
+ * @ciclo 2025-1
+ * 
+ */
+public class PuntoDeVentaBLRV {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto en proceso de construcción...");
+    }
+}
